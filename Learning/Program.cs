a65aa222a5a4032cac0d8ba44f0bda79bf2fcbe7
GabrielOliveS <gabriel.oliv.silva@hotmail.com>
2023-11-08 20:58:37 -0300

    Projeto inicial - testando funções

---
+++ Learning/Program.cs
@@ -0,0 +1,21 @@
+﻿// Inciando processo de aprendizagem
+
+using System.Globalization;
+
+namespace Learning
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+             
+            var time = DateTime.UtcNow.ToString("dd-M-yyyy");
+            Console.WriteLine($"{time}");
+        }
+
+    }
+
+}
+
+
+
