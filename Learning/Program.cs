fa59d3121bcb656637fffeaf6f65fa9d8967b5f3
GabrielOliveS <gabriel.oliv.silva@hotmail.com>
2023-11-08 22:13:41 -0300

    Aprendendo conceitos de soma aplicada

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -8,9 +8,31 @@ namespace Learning
     {
         public static void Main(string[] args)
         {
+            bool erro = true;
+            string valor1 = null;
+            string valor2 = null;
+
+            while (erro == true) 
+            {
+                Console.Write("Digite o primeiro valor: ");
+                valor1 = Console.ReadLine();
+                Console.Write("Digite o segundo valor: ");
+                valor2 = Console.ReadLine();
+                if (valor1 == null || valor2 == null || valor1 == "" || valor2 == "") 
+                {
+                    Console.WriteLine("Escreva um valor válido");
+                }
+                else
+                {
+                    erro = false;
+                }
+           
+                
+            }
+            var soma = int.Parse(valor1) + int.Parse(valor2);
+            Console.WriteLine(soma);
+            Console.ReadKey();
              
-            var time = DateTime.UtcNow.ToString("dd-M-yyyy");
-            Console.WriteLine($"{time}");
         }
 
     }
